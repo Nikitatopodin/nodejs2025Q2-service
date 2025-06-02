632d859bb7c30ec9d15fa13d4aefd51b7bf74f46
Никита Быковский <bykovskii@trueconf.ru>
2025-06-02 23:24:26 +0300

    feat: add docker and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { FavoritesModule } from './favorites/favorites.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user123',
+      password: 'password123',
+      database: 'home-library-service',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     UserModule,
     ArtistModule,
     TrackModule,
